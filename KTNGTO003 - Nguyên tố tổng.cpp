3b9c22b1f0e7a5c63a698c35c8cba609e560b0c6
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-10-18 23:31:50 +0700

    Nguyên tố tổng

---
+++ KTNGTO003 - Nguyên tố tổng.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool Prime(long long n) {
+    if(n<=1) 
+        return false;
+    if(n<=3) 
+        return true;
+    if(n%2 == 0 || n%3 == 0) 
+        return false;
+    for(long long i = 5; i*i <= n; i++) {
+        if(n%i==0|| n%(i+2)== 0) 
+            return false;
+    }
+    return true;
+}
+long long isPrime(long long n){
+    if(n<=1) 
+        return 5;
+    long long prime = n-1;
+    bool check = false;
+    while(!check) {
+        prime++;
+        if(Prime(prime) && Prime(prime)) 
+            check = true;
+    }
+    return prime;
+}
+int main () {
+    int tc; cin>>tc;
+    while(tc--) {
+        long long n;
+        cin >> n;
+        cout << isPrime(n) << endl;
+    }
+    return 0;
+}
