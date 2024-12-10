371c7babef40b1bc3ecb502abb05eb6934227b2b
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-11 00:06:06 +0700

    Update KTBDS002 - Biểu diễn số 02.cpp

--- KTBDS002 - Biểu diễn số 02.cpp
+++ KTBDS002 - Biểu diễn số 02.cpp
@@ -1,25 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void solve() {
-    int n,k; cin>>n>>k;
-    long long mang[k+1][n+1]; // tạo mảng để lưu trữ
-    for(int i = 0; i<=n; i++) {
-        mang[1][i] = 1; // khi k=0 thì chỉ có 1 cách phân chia duy nhất
-    }
-    for(int i = 2; i<=k ;i++) {
-        long long sum = mang[i-1][0];
-        mang[i][0] = mang[i-1][0];
-        for(int j = 1; i<=n; j++) {
-            sum += mang[i-1][j];
-            mang[i][j] = sum;
+#define MOD 1000000007
+#define MAX 100001
+using ll = long long;
+
+void solve()
+{
+    ll n,k;
+    cin >> n >> k;
+    ll dp[k+1][n+1];
+    for(int i = 0; i <= n; i++)
+        dp[1][i] = 1;
+    for(int i = 2; i <= k; i++)
+    {
+        ll sum = dp[i - 1][0];
+        dp[i][0] = dp[i - 1][0];
+        for(int j = 1; j <= n; j++)
+        {
+            sum += dp[i-1][j];
+            dp[i][j] = sum;
         }
     }
-    cout<<mang[k][n]<<endl;
+    cout << dp[k][n] << endl;
 }
-int main () {
-    int tc; cin>>tc;
-    while(tc--) {
-        solve();
-    }
-    return 0;
+
+int main()
+{
+    int t; cin >> t;
+    while(t--) solve();
 }
