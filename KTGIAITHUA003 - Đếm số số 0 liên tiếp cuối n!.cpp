b9a8bc7bb8b63f1e010decbb228bac598178026d
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-18 11:01:51 +0700

    Update KTGIAITHUA003 - Đếm số số 0 liên tiếp cuối n!.cpp

--- KTGIAITHUA003 - Đếm số số 0 liên tiếp cuối n!.cpp
+++ KTGIAITHUA003 - Đếm số số 0 liên tiếp cuối n!.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-int dem (long long n) {
-    int count = 0;
-    for(int i = 5; i<= n ;i*= 5) {
+long long dem (long long n) {
+    long long count = 0;
+    for(long long i = 5; i<= n ;i*= 5) {
         count += n/i;
     }
     return count;
