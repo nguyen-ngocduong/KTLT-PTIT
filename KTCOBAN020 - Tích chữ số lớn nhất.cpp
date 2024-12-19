abeeda36bc53e7aa2977bd24cdeeef2199dda168
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-19 23:34:41 +0700

    Update KTCOBAN020 - Tích chữ số lớn nhất.cpp

--- KTCOBAN020 - Tích chữ số lớn nhất.cpp
+++ KTCOBAN020 - Tích chữ số lớn nhất.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 unsigned long long solve(unsigned long long n) {
     if(n == 0) return 1;
-    if(n<=10) return n;
+    if(n<10) return n;
     return max(solve(n/10)*(n%10), solve(n/10 - 1)*9);
 }
 int main () {
