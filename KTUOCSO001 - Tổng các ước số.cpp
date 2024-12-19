b1de39cd3474745cb0885676f9eef535a6752867
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-19 11:31:57 +0700

    Update KTUOCSO001 - Tổng các ước số.cpp

--- KTUOCSO001 - Tổng các ước số.cpp
+++ KTUOCSO001 - Tổng các ước số.cpp
@@ -1,23 +1,45 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// long long tong_uoc_so(long long n) {
+//     long long sum = 1;
+//     if(n==1) return 0;
+//     for(int i = 2; i<= sqrt(n); i++) {
+//         if(n%i==0) {
+//             if(i*i != n) {
+//                 sum += i+n/i;
+//             }
+//             else sum += i;
+//         }
+//     }
+//     return sum;
+// }
+// int main () {
+//     int tc; cin>>tc;
+//     while(tc--) {
+//         long long n; cin>>n;
+//         cout<<tong_uoc_so(n)<<endl;
+//     }
+//     return 0;
+// }
 #include<bits/stdc++.h>
 using namespace std;
-long long tong_uoc_so(long long n) {
-    long long sum = 1;
-    if(n==1) return 0;
-    for(int i = 2; i<= sqrt(n); i++) {
-        if(n%i==0) {
-            if(i*i != n) {
-                sum += i+n/i;
-            }
-            else sum += i;
-        }
-    }
-    return sum;
-}
 int main () {
     int tc; cin>>tc;
     while(tc--) {
         long long n; cin>>n;
-        cout<<tong_uoc_so(n)<<endl;
+        long long sum = 1;
+        for(int i = 2; i*i<= n; i++) {
+            if(n%i==0) {
+                if(i != n/i) {
+                    sum += i+n/i;
+                }
+                else sum += i;
+            }
+        }
+        if(n == 1) {
+            sum = 0;
+        }
+        cout<<sum<<endl;
     }
     return 0;
 }
