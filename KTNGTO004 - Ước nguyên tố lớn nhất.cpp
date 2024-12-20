26df466f4f156bf25c2c5425d1aa56a13c65bb47
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-20 12:42:34 +0700

    Update KTNGTO004 - Ước nguyên tố lớn nhất.cpp

--- KTNGTO004 - Ước nguyên tố lớn nhất.cpp
+++ KTNGTO004 - Ước nguyên tố lớn nhất.cpp
@@ -1,38 +1,34 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-long long max_prime(long long n) {
-    if (n < 2) return -1; // Tr? v? -1 n?u n < 2
-
-    long long max_prime_factor = -1;
-
-    // Ki?m tra 2 trý?c ð? lo?i b? t?t c? các s? ch?n
-    while (n % 2 == 0) {
-        max_prime_factor = 2;
+long long max_uoc(long long n) {
+    vector<long long> v;
+    while(n%2 == 0) {
+        v.push_back(2);
         n /= 2;
     }
-
-    // Ki?m tra các s? l? t? 3 ð?n sqrt(n)
-    for (long long i = 3; i <= sqrt(n); i += 2) {
-        while (n % i == 0) {
-            max_prime_factor = i;
+    while(n%3 == 0) {
+        v.push_back(3);
+        n /= 3;
+    }
+    for(int i = 5; i<=sqrt(n); i+= 6) {
+        while(n%i == 0) {
+            v.push_back(i);
             n /= i;
         }
+        while(n%(i+2) == 0) {
+            v.push_back(i+2);
+            n /= (i+2);
+        }
     }
-
-    // N?u n c?n l?i l?n hõn 2, nó là s? nguyên t? l?n nh?t
-    if (n > 1) max_prime_factor = n;
-
-    return max_prime_factor;
+    if(n) v.push_back(n);
+    sort(v.begin(), v.end());
+    return v[v.size() - 1];
 }
-
-int main() {
-    int tc; 
-    cin >> tc;
-    while (tc--) {
-        long long n;
-        cin >> n;
-        cout << max_prime(n) << endl; 
+int main () {
+    int tc; cin >> tc;
+    while(tc--) {
+        long long n; cin >> n;
+        cout << max_uoc(n) << endl;
     }
-    return 0; 
-} 
+    return 0;
+}
