1badb58e40dc15c8c1fe958166e9efcfa6262ce9
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-21 00:31:01 +0700

    Update KTCOBAN016 - Binh phuong so lon.cpp

--- KTCOBAN016 - Binh phuong so lon.cpp
+++ KTCOBAN016 - Binh phuong so lon.cpp
@@ -1,49 +1,76 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// void so_lon_hon9(int n, bool mot = 1){
+//     if(n > 9)
+// 	{
+// 		cout << "123456790";
+// 		so_lon_hon9(n-9, 0);
+// 		cout << "098765432";
+// 		if (mot) cout << 1;
+// 	}
+// 	else {
+// 		if (n==1) return;
+// 		for (int i = 1; i <= n; i++)
+// 		{
+// 			cout << i;
+// 		}
+// 		for (int i = n-1; i > 1; i--)
+// 		{
+// 			cout << i;
+// 		}
+// 		return;
+// 	}
+// }
+// void so_nho_hon9(int n) {
+//     if (n == 1)
+// 	{
+// 		cout << 1;
+// 		return;
+// 	}
+// 	for (int i = 1; i<= n; i++)
+// 	{
+// 		cout << i;
+// 	}
+// 	for (int i = n-1; i > 0; i--)
+// 	{
+// 		cout << i;
+// 	}
+// 	return;
+// }
+// int main () {
+//     int tc; cin>>tc;
+//     while(tc--) {
+//         int n; cin>>n;
+//         if(n>9) so_lon_hon9(n);
+//         else so_nho_hon9(n);
+//         cout<<endl;
+//     }
+//     return 0;
+// }
 #include<bits/stdc++.h>
+
 using namespace std;
-void so_lon_hon9(int n, bool mot = 1){
-    if(n > 9)
-	{
-		cout << "123456790";
-		so_lon_hon9(n-9, 0);
-		cout << "098765432";
-		if (mot) cout << 1;
-	}
-	else {
-		if (n==1) return;
-		for (int i = 1; i <= n; i++)
-		{
-			cout << i;
+
+int main(){
+	int t;
+	cin >> t;
+	while(t--){
+		int n;
+		cin >> n;
+		for(int i = 1; i <= (n-1)/9 ; i++){
+			cout<<"123456790";
 		}
-		for (int i = n-1; i > 1; i--)
-		{
-			cout << i;
+		int k = (n-1)%9;
+		for(int i = 1; i <= k ;i++){
+			cout << char('0'+i);
 		}
-		return;
+		for(int i = k + 1; i >= 2; i--){
+			cout << char('0'+i);
+		}
+		for(int i = 1; i <= (n-1)/9; i++){
+			cout<<"098765432";
+		}
+		cout<<1<<"\n";
 	}
-}
-void so_nho_hon9(int n) {
-    if (n == 1)
-	{
-		cout << 1;
-		return;
-	}
-	for (int i = 1; i<= n; i++)
-	{
-		cout << i;
-	}
-	for (int i = n-1; i > 0; i--)
-	{
-		cout << i;
-	}
-	return;
-}
-int main () {
-    int tc; cin>>tc;
-    while(tc--) {
-        int n; cin>>n;
-        if(n>9) so_lon_hon9(n);
-        else so_nho_hon9(n);
-        cout<<endl;
-    }
-    return 0;
+	return 0;
 }
