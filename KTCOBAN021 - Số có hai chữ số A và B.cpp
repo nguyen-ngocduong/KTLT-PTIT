c6259f720aca7d1345ddb6d6902c1d8c1b741898
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-21 04:45:13 +0700

    Update KTCOBAN021 - Số có hai chữ số A và B.cpp

--- KTCOBAN021 - Số có hai chữ số A và B.cpp
+++ KTCOBAN021 - Số có hai chữ số A và B.cpp
@@ -6,7 +6,7 @@ output*10+x thêm x vào output => số lượng x_count tăng 1
 output*10+y thêm y vào out put => số lượng y_count tăng 1*/
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 10000000000000001
+#define MAX 1000000000000000001
 typedef unsigned long long ull;
 ull hamdequy(ull output, short x, ull x_count, short y, ull y_count, ull n, ull sochuso) {
     if(x_count + y_count > sochuso + 2 || output > MAX){
@@ -22,7 +22,7 @@ ull timso(ull n, short x, short y){
     ull output = 0, x_count = 0, y_count = 0;
     int sochuso = 1 + floor(log10(n));
     if(x == y) {
-        while(output > n) {
+        while(output < n) {
             output = output * 10 + x;
         }
         return output;
