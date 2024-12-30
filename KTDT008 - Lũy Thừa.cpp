4f1faa78c5a139d82e6a3efac926219e9c2ae0a8
nguyen-ngocduong <nguyenngocduongn2004@gmail.com>
2024-12-31 03:35:59 +0700

    Create KTDT008 - Lũy Thừa.cpp

---
+++ KTDT008 - Lũy Thừa.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int st, t;
+	cin >> st;
+	for(t=1;t<=st;++t) {
+		long long L, R;
+		cin >> L >> R;
+		for(int k=40;k>=1;--k) {
+			long long left = 0, right = (long long)(pow( R, 1.0 / k) + 10);
+			while(left<right) {
+				long long mid = (left+right)/2;
+				long long z = 1;
+				for(int i=0;i<k;++i) {
+					z *= mid;
+					if(z>=L) break;
+				}
+				if(z>=L) right = mid;
+				else left = mid+1;
+			}
+			long long z = 1;
+			for(int i=0;i<k;++i) {
+				z *= left;
+				if(z>R) break;
+			}
+			if(L<=z && z<=R) {
+				cout << k << endl;
+				break;
+			}
+		}
+	}
+	return 0;
+}
